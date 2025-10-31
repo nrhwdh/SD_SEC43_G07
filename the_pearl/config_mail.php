d3288dfe414522d7045999c0cfc0486caf246ef3
nrhwdh <nrhwaidh@gmail.com>
2025-10-31 11:46:22 +0800

    REMOVE ALL PROJWCT FILWS

--- the_pearl/config_mail.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Email configuration for The Pearl Hotel
-define('HOTEL_EMAIL', 'noreply.thepearlhotel@gmail.com'); // email you baru tu
-define('HOTEL_NAME', 'The Pearl Hotel');
-define('HOTEL_APP_PASSWORD', 'dcmokhagxrjfhtps'); // <-- paste 16-character code dari Google App Password
-?>
