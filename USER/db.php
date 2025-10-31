e6db83e3e02b7357b1a584e991b1fbc48d76cd6f
nrhwdh <nrhwaidh@gmail.com>
2025-10-31 14:48:26 +0800

    UPDATE LATEST

--- USER/db.php
+++
@@ -1,8 +0,0 @@
-<?php
-$host = "localhost";
-$user = "root";
-$pass = "";
-$dbname = "the_pearl";
-$conn = new mysqli($host, $user, $pass, $dbname);
-if ($conn->connect_error) { die("Connection failed: " . $conn->connect_error); }
-?>
